005c5beecf4841b6318d131df07c5484112f938a
mmkhajoo <mmkhajoo@gmail.com>
2022-03-01 16:59:50 +0330

    Data Layer Added.

---
+++ Data/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Data
+{
+    public class User : BaseEntity
+    {
+        public string UserName { get; set; }  
+        public string Email { get; set; }  
+        public string Password { get; set; }  
+        public virtual UserProfile UserProfile { get; set; }  
+    }
+}
